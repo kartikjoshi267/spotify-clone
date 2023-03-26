76e1a6901be8fb2978495be762b52d67e075060c
Rishabh Sinha <sinharishabh402@gmail.com>
2023-03-26 23:10:25 +0530

    Library Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Library from './pages/Library'
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Library />
     </div>
   )
 }
